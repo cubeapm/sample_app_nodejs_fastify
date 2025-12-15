7cc7d9e76f6289b0df94529444a3ee8b19984c24
ishaan766 <ishaan766@gmail.com>
2025-12-15 10:50:19 +0530

    send logs to CubeAPM

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const Fastify = require("fastify");
 const axios = require("axios");
 const mysql = require("mysql2");
 const redis = require("redis");
+const { createLogger, format, transports } = require('winston');
+const winston = require("winston");
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+  ],
+});
+
+module.exports = logger;
 
 const start = async () => {
   const app = Fastify({ logger: true });
@@ -25,22 +37,37 @@ const start = async () => {
   console.log("redis connected!");
 
   // --- Routes ---
-  app.get("/", async () => "Hello");
+    app.get("/", async () => {
+    logger.info("root called");
+    return "Hello"
+  });
 
-  app.get("/param/:param", async (req) => `Got param ${req.params.param}`);
+  app.get("/param/:param", async (req) => {
+    logger.info("param called");
+    return `Got param ${req.params.param}`;
+  });
 
-  app.get("/exception", async () => { throw new Error("Sample exception"); });
+  app.get("/exception", async () => {
+    logger.info("exception called");
+    throw new Error("Sample exception");
+  });
 
-  app.get("/api", async () => { await axios.get("http://localhost:8000/"); return "API called"; });
+  app.get("/api", async () => { 
+    logger.info("api called");
+    await axios.get("http://localhost:8000/"); return "API called"; });
 
   app.get("/mysql", async () => new Promise((resolve, reject) => {
+    logger.info("mysql called");
     mysqlClient.query("SELECT NOW()", (err, results) => {
       if (err) reject(err);
       else resolve(results[0]["NOW()"]);
     });
   }));
 
-  app.get("/redis", async () => { await redisClient.set("foo", "bar"); return "Redis called"; });
+  app.get("/redis", async () => { 
+    logger.info("redis called");
+    await redisClient.set("foo", "bar"); return "Redis called"; 
+  });
 
   // --- Start server ---
   const PORT = parseInt(process.env.PORT || "8000");
